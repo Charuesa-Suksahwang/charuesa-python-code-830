60038eb2345674afb165b737bd6e12e4679c734f
Charuesa Suksahwang <charuesa.s@ku.th>
2025-09-23 11:52:27 +0700

    quiz_week_9

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,24 @@
 import random
 
 def test_random():
-    random_number = random.randint(1, 100)
+    random_number = random.randint(1, 10)
+
+    user_number = int(input("Enter your number: "))
+
+    if random_number == user_number:
+        print("Congratulation")
+
+    elif random_number < user_number:
+        print("Too mush")
+
+    elif random_number > user_number:
+        print("Too low")
+
+    else:
+        print("Try again")
+
     print(random_number)
     
 test_random()
+
+
